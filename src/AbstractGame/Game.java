b8e642c3757e2229aac723be65abdc7a9222a148
HEROgold <martijnwieringa28@gmail.com>
2024-09-12 15:33:41 +0200

    meer code

--- src/AbstractGame/Game.java
+++ src/AbstractGame/Game.java
@@ -3,8 +3,13 @@ package AbstractGame;
 public class Game {
     private Player[] players;
     private LeaderBoard leaderBoard;
+    private Timer timer;
 
     public Game(Player[] players) {
         this.players = players;
+    }
+
+    public void startGame() {
+        // Start the game
     }
 }
--- src/AbstractGame/State.java
+++ src/AbstractGame/State.java
@@ -1,5 +1,7 @@
 package AbstractGame;
 
 public class State {
-    
+    private int turn;
+    private boolean isGameOver;
+    private Player[] players;
 }
