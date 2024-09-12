8e1276694b414a908246a6dfb18ce1669f80cf28
HEROgold <martijnwieringa28@gmail.com>
2024-09-12 15:35:54 +0200

    constructor

--- src/AbstractGame/State.java
+++ src/AbstractGame/State.java
@@ -4,4 +4,10 @@ public class State {
     private int turn;
     private boolean isGameOver;
     private Player[] players;
+
+    public State(Player[] players) {
+        this.turn = 0;
+        this.isGameOver = false;
+        this.players = players;
+    }
 }
