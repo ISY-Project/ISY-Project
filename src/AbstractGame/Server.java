bfc2fabf2284775e979aed0366361c3a77480f9c
HEROgold <martijnwieringa28@gmail.com>
2024-09-15 14:11:17 +0200

    add server

---
+++ src/AbstractGame/Server.java
@@ -0,0 +1,19 @@
+package AbstractGame;
+
+public class Server {
+    public boolean connect(){
+        return true;
+    }
+
+    public boolean disconnect(){
+        return true;
+    }
+
+    public boolean send(){
+        return true;
+    }
+
+    public boolean receive(){
+        return true;
+    }
+}
