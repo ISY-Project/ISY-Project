1cd0528515439be1e4c8ec0f7da34c70c048311c
HEROgold <martijnwieringa28@gmail.com>
2024-09-17 13:52:34 +0200

    add ishit

--- src/Battleships/Heatmap.java
+++ src/Battleships/Heatmap.java
@@ -4,4 +4,21 @@ public class Heatmap {
     Heatmap(int size) {
         heatmap = new int[size][size];
     }
+
+    public void setHeat(int x, int y, int heat) {
+        heatmap[x][y] = heat;
+    }
+
+    public void addHeat(int x, int y, int heat) {
+        heatmap[x][y] += heat;
+    }
+
+    public void subtractHeat(int x, int y, int heat) {
+        heatmap[x][y] -= heat;
+    }
+
+    public int getHeat(int x, int y) {
+        return heatmap[x][y];
+    }
+
 }
--- src/Battleships/Ship.java
+++ src/Battleships/Ship.java
@@ -12,4 +12,22 @@ public class Ship {
         this.direction = direction;
         this.hits = new boolean[size];
     }
+
+    public boolean isHit(int x, int y) {
+        if (y == this.y && x >= this.x && x < this.x + size) {
+                hits[x - this.x] = true;
+                return true;
+        } else if (x == this.x && y >= this.y && y < this.y + size) {
+                hits[y - this.y] = true;
+                return true;
+        }
+        if (x == this.x && y >= this.y && y < this.y + size) {
+                hits[y - this.y] = true;
+                return true;
+        } else if (y == this.y && x >= this.x && x < this.x + size) {
+                hits[x - this.x] = true;
+                return true;
+        }
+        return false;
+    }
 }
