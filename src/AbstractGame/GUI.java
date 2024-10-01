5624d037662c8825c1f1444349db6b1d0310567b
HEROgold <martijnwieringa28@gmail.com>
2024-10-01 13:58:13 +0200

    we have working GUI. I think?

--- src/AbstractGame/GUI.java
+++ src/AbstractGame/GUI.java
@@ -3,5 +3,38 @@ package AbstractGame;
 public class Gui {
     public void show() {
         System.out.println("Showing GUI");
+
+        int[][] grid = new int[][] {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        // Print column numbers
+        System.out.print("  ");
+        for (int i = 0; i < grid[0].length; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Print rows with letters
+        char rowLabel = 'A';
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print(rowLabel + " ");
+            rowLabel++;
+            for (int j = 0; j < grid[i].length; j++) {
+            System.out.print(Blocks.values()[grid[i][j]] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+        gui.show();
     }
 }
