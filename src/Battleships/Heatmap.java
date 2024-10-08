7f194736da9d78babfc6aa2cc570528a772ae04d
HEROgold <martijnwieringa28@gmail.com>
2024-10-08 13:27:41 +0200

    visuallize a heat

--- src/Battleships/Heatmap.java
+++ src/Battleships/Heatmap.java
@@ -5,6 +5,18 @@ public class Heatmap {
         heatmap = new int[size][size];
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : heatmap) {
+            for (int cell : row) {
+                sb.append(cell).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     public void setHeat(int x, int y, int heat) {
         heatmap[x][y] = heat;
     }
@@ -21,4 +33,25 @@ public class Heatmap {
         return heatmap[x][y];
     }
 
+    public static void main(String[] args) {
+        int size = 8;
+        var heatmap = new Heatmap(size);
+
+        // Initialize the heatmap with some default values
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                heatmap.setHeat(i, j, 1); // Start with a base heat of 1 for all cells
+            }
+        }
+
+        // Add heat to the cells based on their distance to the corners
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+            int distanceToCorner = Math.min(Math.min(i, size - 1 - i), Math.min(j, size - 1 - j));
+            heatmap.addHeat(i, j, distanceToCorner);
+            }
+        }
+
+        System.out.println(heatmap);
+    }
 }
