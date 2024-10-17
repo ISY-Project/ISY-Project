9abe7a71e1560c99d9bc33320a23ae81a5769f14
HEROgold <martijnwieringa28@gmail.com>
2024-10-17 12:44:56 +0200

    remove todo

--- src/Telnet/TelnetClient.java
+++ src/Telnet/TelnetClient.java
@@ -49,8 +49,6 @@ public class TelnetClient {
     }
 
     public static void main(String[] args) {
-        // Test example
-        // TODO Remove when it works
         TelnetClient client = new TelnetClient();
         try {
             client.connect("localhost", 7789); // Replace with your server and port
