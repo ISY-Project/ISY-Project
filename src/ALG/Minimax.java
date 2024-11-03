8c4656cf74145f32f95c068b710fe61f6cadce0f
HEROgold <martijnwieringa28@gmail.com>
2024-11-03 15:05:59 +0100

    fix AND vs Bitwise And

--- src/ALG/Minimax.java
+++ src/ALG/Minimax.java
@@ -8,17 +8,17 @@ public class Minimax {
     private static int bestMove;
 
     private static GameState gameResult(char[] grid) {
-        if ((grid[0] == 'X' & grid[1] == 'X' & grid[2] == 'X') | (grid[3] == 'X' & grid[4] == 'X' & grid[5] == 'X') | (grid[6] == 'X' & grid[7] == 'X' & grid[8] == 'X') | 
-            (grid[0] == 'X' & grid[3] == 'X' & grid[6] == 'X') | (grid[1] == 'X' & grid[4] == 'X' & grid[7] == 'X') | (grid[2] == 'X' & grid[5] == 'X' & grid[8] == 'X') | 
-            (grid[0] == 'X' & grid[4] == 'X' & grid[8] == 'X') | (grid[2] == 'X' & grid[4] == 'X' & grid[6] == 'X')) {
+        if ((grid[0] == 'X' && grid[1] == 'X' && grid[2] == 'X') | (grid[3] == 'X' && grid[4] == 'X' && grid[5] == 'X') | (grid[6] == 'X' && grid[7] == 'X' && grid[8] == 'X') | 
+            (grid[0] == 'X' && grid[3] == 'X' && grid[6] == 'X') | (grid[1] == 'X' && grid[4] == 'X' && grid[7] == 'X') | (grid[2] == 'X' && grid[5] == 'X' && grid[8] == 'X') | 
+            (grid[0] == 'X' && grid[4] == 'X' && grid[8] == 'X') | (grid[2] == 'X' && grid[4] == 'X' && grid[6] == 'X')) {
             return GameState.WIN_X;
         }
-        if ((grid[0] == 'O' & grid[1] == 'O' & grid[2] == 'O') | (grid[3] == 'O' & grid[4] == 'O' & grid[5] == 'O') | (grid[6] == 'O' & grid[7] == 'O' & grid[8] == 'O') | 
-            (grid[0] == 'O' & grid[3] == 'O' & grid[6] == 'O') | (grid[1] == 'O' & grid[4] == 'O' & grid[7] == 'O') | (grid[2] == 'O' & grid[5] == 'O' & grid[8] == 'O') | 
-            (grid[0] == 'O' & grid[4] == 'O' & grid[8] == 'O') | (grid[2] == 'O' & grid[4] == 'O' & grid[6] == 'O')) {
+        if ((grid[0] == 'O' && grid[1] == 'O' && grid[2] == 'O') | (grid[3] == 'O' && grid[4] == 'O' && grid[5] == 'O') | (grid[6] == 'O' && grid[7] == 'O' && grid[8] == 'O') | 
+            (grid[0] == 'O' && grid[3] == 'O' && grid[6] == 'O') | (grid[1] == 'O' && grid[4] == 'O' && grid[7] == 'O') | (grid[2] == 'O' && grid[5] == 'O' && grid[8] == 'O') | 
+            (grid[0] == 'O' && grid[4] == 'O' && grid[8] == 'O') | (grid[2] == 'O' && grid[4] == 'O' && grid[6] == 'O')) {
             return GameState.WIN_O;
         }
-        if (grid[0] != ' ' & grid[1] != ' ' & grid[2] != ' ' & grid[3] != ' ' & grid[4] != ' ' & grid[5] != ' ' &grid[6] != ' ' & grid[7] != ' ' & grid[8] != ' ') {
+        if (grid[0] != ' ' && grid[1] != ' ' && grid[2] != ' ' && grid[3] != ' ' && grid[4] != ' ' && grid[5] != ' ' &&grid[6] != ' ' && grid[7] != ' ' && grid[8] != ' ') {
             return GameState.TIE;
         }
         return GameState.ONGOING;
