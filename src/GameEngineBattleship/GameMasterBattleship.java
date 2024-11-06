69583daafb45459f226b4ad2dc734e714bed2181
HEROgold <martijnwieringa28@gmail.com>
2024-11-06 23:35:59 +0100

    get heatmap

--- src/GameEngineBattleship/GameMasterBattleship.java
+++ src/GameEngineBattleship/GameMasterBattleship.java
@@ -13,6 +13,10 @@ public class GameMasterBattleship{
     private Heatmap heatmap;
     // private int[] enemy_ship_locs; //TODO need this later, not for vrijdag/comp build
     
+    public Heatmap getHeatmap() {
+        return heatmap;
+    }
+
     public GameMasterBattleship(int [] gridShape, int[] ships, boolean[] rules){
 
         this.gridShape = gridShape;
