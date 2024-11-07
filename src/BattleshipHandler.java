13c8c42e08ada8c39dbc2a6fe64274c20fd2a4eb
HEROgold <martijnwieringa28@gmail.com>
2024-11-07 19:45:04 +0100

    fix

--- src/BattleshipHandler.java
+++ src/BattleshipHandler.java
@@ -24,7 +24,7 @@ public class BattleshipHandler extends EventHandler {
     private boolean placeStage = true;
 
     public BattleshipHandler() {
-        super(GameType.BATTLESHIP);
+        super(GameType.Battleship);
         this.mainFrame = Main.getMainFrame();
         this.battleshipGUI = mainFrame.getBattleshipGUI();
         this.playerGrid = battleshipGUI.getPlayerGrid();
@@ -55,7 +55,7 @@ public class BattleshipHandler extends EventHandler {
             Main.getMainFrame().showScreen(Screens.BATTLESHIP);
             mainFrame.setAlgorithmOn(true);
         }
-        Main.setGameType(GameType.BATTLESHIP);
+        Main.setGameType(GameType.Battleship);
         Main.toggleBattleshipGrid();
         playerGrid.resetGrid();
         opponentGrid.resetGrid();
