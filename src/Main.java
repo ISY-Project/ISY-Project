5d244ddb4eeaa3b1aecb0fb836e412598fb274bf
HEROgold <martijnwieringa28@gmail.com>
2024-11-07 18:53:17 +0100

    re-add fixed Firstboot

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
     // BattleshipEngine(8, "test", "test2");
     private static final BattleshipHandler battleshipHandler = new BattleshipHandler();
     private static final ResponseHandler battleshipResponseHandler = new ResponseHandler(battleshipHandler);
-    private static GameType gameType = GameType.NONE;
+    private static GameType gameType = GameType.FIRSTBOOT;
 
     public static GameType getGameType() {
         return gameType;
