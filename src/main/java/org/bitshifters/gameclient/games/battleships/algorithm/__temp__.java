b72a2ac84194bc5d3b25cc89a0e7e0702b82f7a3
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-14 10:59:44 +0100

    Created more temp classes
    created more temp classes in order to have the correct folders show up in GitHub

---
+++ src/main/java/org/bitshifters/gameclient/games/battleships/algorithm/__temp__.java
@@ -0,0 +1,5 @@
+package org.bitshifters.gameclient.games.battleships.algorithm;
+
+public class __temp__ {
+
+}
---
+++ src/main/java/org/bitshifters/gameclient/games/tictactoe/algorithm/__temp__.java
@@ -0,0 +1,5 @@
+package org.bitshifters.gameclient.games.tictactoe.algorithm;
+
+public class __temp__ {
+
+}
