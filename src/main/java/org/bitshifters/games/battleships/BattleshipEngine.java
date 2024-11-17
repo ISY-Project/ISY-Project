c4a59dadabdfa2d8348e0a62a8a77c43b47fa416
HEROgold <martijnwieringa28@gmail.com>
2024-11-17 01:14:19 +0100

    add and track ships

--- src/main/java/org/bitshifters/games/battleships/BattleshipEngine.java
+++ src/main/java/org/bitshifters/games/battleships/BattleshipEngine.java
@@ -1,11 +1,14 @@
 package org.bitshifters.games.battleships;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.bitshifters.games.components.GridEngine;
 import org.bitshifters.games.components.Player;
 
 public class BattleshipEngine extends GridEngine<BattleshipCell> {
+    private final HashMap<Player, List<Ship>> placedShips;
     int rows;
     int cols;
 
@@ -13,6 +16,35 @@ public class BattleshipEngine extends GridEngine<BattleshipCell> {
         this.rows = rows;
         this.cols = cols;
         this.grids = new HashMap<>();
+        this.placedShips = new HashMap<>();
+    }
+
+    public void placeShip(final int row, final int col, final int length, final boolean horizontal, final Player player) {
+        final Ship ship = new Ship(row, col, length, horizontal);
+        addShip(ship, player);
+        for (int i = 0; i < length; i++) {
+            final var x = horizontal ? row : row + i;
+            final var y = horizontal ? col + i : col;
+            setCell(x, y, BattleshipCell.SHIP, player);
+        }
+    }
+
+    /**
+     * Validate the shot location for the player.
+     */
+    public boolean validatePlacementCell(final int row, final int col, final Player player) {
+        boolean valid = true;
+        if (placedShips.containsKey(player)) {
+            valid = false;
+        }
+        if (row < 0 || row >= rows || col < 0 || col >= cols) {
+            valid = false;
+        }
+        final var cell = getCell(row, col, player);
+        if (cell.contains(BattleshipCell.SHIP)) {
+            valid = false;
+        }
+        return valid;
     }
 
     /**
@@ -28,5 +60,15 @@ public class BattleshipEngine extends GridEngine<BattleshipCell> {
             valid = false;
         }
         return valid;
+    }
+
+    private void addShip(final Ship ship, final Player player) {
+        var playerShips = placedShips.get(player);
+        if (playerShips == null) {
+            playerShips = new ArrayList<>();
+            placedShips.put(player, playerShips);
+        } else {
+            playerShips.add(ship);
+        }
     }
 }
---
+++ src/main/java/org/bitshifters/games/battleships/Ship.java
@@ -0,0 +1,59 @@
+package org.bitshifters.games.battleships;
+
+public class Ship {
+    private final int row;
+    private final int col;
+    private final int length;
+    private final boolean horizontal;
+    private int hits = 0;
+
+    public Ship(final int row, final int col, final int length, final boolean horizontal) {
+        this.row = row;
+        this.col = col;
+        this.length = length;
+        this.horizontal = horizontal;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isHorizontal() {
+        return horizontal;
+    }
+
+    public boolean isVertical() {
+        return !horizontal;
+    }
+
+    public boolean isAt(final int x, final int y) {
+        if (horizontal) {
+            return y == this.col && x >= this.row && x < this.row + length;
+        } else {
+            return x == this.row && y >= this.col && y < this.col + length;
+        }
+    }
+
+    public boolean isHit(final int x, final int y) {
+        if (isAt(x, y)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSunk() {
+        return hits == length;
+    }
+
+    public void hit() {
+        hits++;
+    }
+}
