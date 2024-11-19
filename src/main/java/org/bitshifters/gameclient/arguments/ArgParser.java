97eddd119f913b4880f9c124d62e74d70787a040
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-19 10:55:10 +0100

    Suppress May Be Final Warning

--- src/main/java/org/bitshifters/gameclient/arguments/ArgParser.java
+++ src/main/java/org/bitshifters/gameclient/arguments/ArgParser.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 
+@SuppressWarnings("FieldMayBeFinal") // <- this is a false positive, the fields are not final because JCommander requires them to be mutable
 public class ArgParser {
     // command line variables
     @Parameter(names = {"-n", "--name"}, description = "Set the name of the player", order=1)
