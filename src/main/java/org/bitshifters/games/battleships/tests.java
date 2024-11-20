f43de0c29ef99f2b70020a152d14f3b994a7b944
HEROgold <martijnwieringa28@gmail.com>
2024-11-20 22:54:56 +0100

    setup test

---
+++ src/main/java/org/bitshifters/games/battleships/tests.java
@@ -0,0 +1,7 @@
+package org.bitshifters.games.battleships;
+
+public class tests {
+    public static void main(String[] args) {
+        var engine = new BattleshipEngine(false, 0, 0, null);
+    }
+}
