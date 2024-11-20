b5e8868e91e8ccd02afba9ae74684e9586d62d15
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-20 15:54:03 +0100

    added properties
    added properties to the project so that they can be read from within the project

--- src/main/java/org/bitshifters/ui/MainFrame.java
+++ src/main/java/org/bitshifters/ui/MainFrame.java
@@ -1,9 +1,12 @@
 package org.bitshifters.ui;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -13,10 +16,22 @@ import javafx.stage.Stage;
 public class MainFrame extends Application {
     @Override
     public void start(Stage stage) {
+        GridPane root = new GridPane(5, 5);
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        root.add(l, 0, 0);
+        try {
+            final Properties properties = new Properties();
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("project.properties"));
+            stage.setTitle("Bitshifters-" + properties.getProperty("version"));
+            Label l2 = new Label("Version " + properties.getProperty("version") + ", artifactId " + properties.getProperty("artifactId") + ".");
+            root.add(l2, 0, 1);
+        } catch (IOException e) {
+            stage.setTitle("Bitshifters");
+        }
+
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
