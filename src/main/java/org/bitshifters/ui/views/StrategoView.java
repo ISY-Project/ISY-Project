63f77e7ad5151c1582d7c333ca042102deb6a5ac
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-21 12:55:20 +0100

    Created stratego Pawns

--- src/main/java/org/bitshifters/ui/views/StrategoView.java
+++ src/main/java/org/bitshifters/ui/views/StrategoView.java
@@ -2,9 +2,12 @@ package org.bitshifters.ui.views;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
 
 import org.bitshifters.ui.MainFrame;
 import org.bitshifters.ui.componenets.BaseGrid;
+import org.bitshifters.ui.enums.Pawns;
 import org.bitshifters.ui.enums.Screens;
 
 import javafx.scene.control.Button;
@@ -41,47 +44,61 @@ public class StrategoView extends BorderPane {
     }
 
     public final void fillGrid() {
+        Random rand = new Random();
+        ArrayList<Pawns> pawns = new ArrayList<>();
+        for (Pawns pawn : Pawns.values()) {
+            for (int i = 0; i < pawn.getAmount(); i++) {
+                pawns.add(pawn);
+            }
+        }
+
+        int buttonSize = 70;
         Button[][] buttonGrid = this.baseGrid.getButtonGrid();
         try {
-            FileInputStream inputBlue = new FileInputStream("src\\main\\resources\\StrategoBlue.png");
-            FileInputStream inputRed = new FileInputStream("src\\main\\resources\\StrategoRed.png");
-            Image imageBlue = new Image(inputBlue);
+            FileInputStream inputRed = new FileInputStream("src\\main\\resources\\images\\StrategoRed.png");
             Image imageRed = new Image(inputRed);
             for (int row = 0; row < this.baseGrid.getGridHeight(); row++) {
                 for (int col = 0; col < this.baseGrid.getGridHeight(); col++) {
                     Button button = buttonGrid[row][col];
                     if (row >= 0 && row <= 3) {
+                        Pawns pawn = pawns.get(rand.nextInt(pawns.size()));
+                        pawns.remove(pawn);
+
+                        FileInputStream inputBlue = new FileInputStream(pawn.getBluePath());
+                        Image imageBlue = new Image(inputBlue);
+
                         ImageView imageView = new ImageView(imageBlue);
-                        imageView.setFitHeight(40);
-                        imageView.setFitWidth(40);
+                        imageView.setFitHeight(buttonSize);
+                        imageView.setFitWidth(buttonSize);
                         button.setGraphic(imageView);
-                    } else if (row >= this.baseGrid.getGridHeight()-4 && row <= this.baseGrid.getGridHeight()-1) {
+                    } else if (row >= 6 && row <= 9) {
                         ImageView imageView = new ImageView(imageRed);
-                        imageView.setFitHeight(40);
-                        imageView.setFitWidth(40);
+                        imageView.setFitHeight(buttonSize);
+                        imageView.setFitWidth(buttonSize);
                         button.setGraphic(imageView);
                     } else if (col >= 2 && col <= 3) {
                         button.setStyle("-fx-background-color: #aaaadd");
                     } else if (col >= 6 && col <= 7) {
                         button.setStyle("-fx-background-color: #aaaadd");
+                    } else {
+                        button.setStyle("-fx-background-color: #aaddaa");
                     }
-                    button.setStyle("-fx-background-color: #aaddaa");
-                    button.setMinSize(40, 40);
-                    button.setMaxSize(40, 40);
+                    button.setMinSize(buttonSize, buttonSize);
+                    button.setMaxSize(buttonSize, buttonSize);
                     button.setOnAction(_ -> {
-                        button.setStyle("-fx-background-color: #ff0000");
+                        // button.setStyle("-fx-background-color: #ff0000");
                     });
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.println("File 'src/main/resources/StrategoRed.png' not found ");
+            System.out.println("File not found: " + e);
             for (Button[] row : buttonGrid) {
                 for (Button cell : row) {
-                    cell.setStyle("-fx-background-color: #0000FF");
-                    cell.setMinSize(40, 40);
-                    cell.setMaxSize(40, 40);
+                    cell.setStyle("-fx-background-color: #aaddaa");
+                    cell.setMinSize(buttonSize, buttonSize);
+                    cell.setMaxSize(buttonSize, buttonSize);
                     cell.setOnAction(_ -> {
-                        cell.setStyle("-fx-background-color: #ff0000");
+                        // cell.setStyle("-fx-background-color: #ff0000");
                     });
                 }
             }
