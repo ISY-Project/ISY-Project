487b63c1e0e4557d8e6ab5ff3fce4cc2b7f9cb30
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-21 12:54:52 +0100

    Increase the display size

--- src/main/java/org/bitshifters/ui/MainFrame.java
+++ src/main/java/org/bitshifters/ui/MainFrame.java
@@ -34,9 +34,10 @@ public class MainFrame extends Application {
         // stage.initStyle(StageStyle.DECORATED); // normal view with status bar and close button
         stage.initStyle(StageStyle.UTILITY); // No status bar and only close button
 
-        Scene scene = new Scene(root, 1000, 600);
+        Scene scene = new Scene(root, 1200, 800);
         this.stage.setScene(scene);
         this.stage.show();
+        // this.stage.setFullScreen(true);
 
         stage.addEventHandler(KeyEvent.KEY_PRESSED,  (event) -> {
             System.out.println("Key pressed: " + event.getCode());
