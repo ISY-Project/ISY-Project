febe206861c08a50e99f5d7ec243089d1ec4dd82
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-24 13:11:39 +0100

    updated the verbose description

--- src/main/java/org/bitshifters/arguments/Flags.java
+++ src/main/java/org/bitshifters/arguments/Flags.java
@@ -20,6 +20,6 @@ public class Flags {
     @Parameter(names = {"-debug", "--debug"}, help = true, description = "Debug output will be shown", order=0)
     public boolean DEBUG = false;
 
-    @Parameter(names = {"-log", "--log", "-verbose", "--verbose"}, converter = VerboseConverter.class, validateWith = VerboseLevelValidator.class, description = "Level of verbosity\n         1 is low to none, 2 is medium, 3 high", order=0)
+    @Parameter(names = {"-log", "--log", "-verbose", "--verbose"}, converter = VerboseConverter.class, validateWith = VerboseLevelValidator.class, description = "Level of verbosity\n         0 is off, 1 is low, 2 is medium, 3 is high,     4 is debug, 5 is all", order=0)
     public VerboseLevel VERBOSE = VerboseLevel.LOW;
 }
