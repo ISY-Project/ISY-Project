bcb6b0341f890b3e177f9e352ae11836bffe482d
HEROgold <martijnwieringa28@gmail.com>
2024-11-25 15:41:40 +0100

    disable v6.

--- src/main/java/org/bitshifters/arguments/validators/IPValidator.java
+++ src/main/java/org/bitshifters/arguments/validators/IPValidator.java
@@ -10,12 +10,14 @@ public class IPValidator implements IParameterValidator {
     @Override
     public void validate(String name, String value) throws ParameterException {
         logger.debug("Validating parameter " + name + " with value " + value);
-        String IPv4_PATTERN = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
-        if (!value.matches(IPv4_PATTERN) && !value.matches("localhost")) {
-            String errorMessage = "Parameter " + name + " should be an IPv4 or IPv6 address (found " + value +")";
-            ParameterException parameterException = new ParameterException(errorMessage);
-            logger.error(parameterException);
-            throw parameterException;
+        String IPv4_PATTERN = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$";
+        // String IPV6_PATTERN = ""; || value.matches(IPV6_PATTERN);
+        if (value.equals("localhost") || value.matches(IPv4_PATTERN)) {
+            return;
         }
+        String errorMessage = "Parameter " + name + " should be an IPv4 or IPv6 address (found " + value +")";
+        ParameterException parameterException = new ParameterException(errorMessage);
+        logger.error(parameterException);
+        throw parameterException;
     }
 }
