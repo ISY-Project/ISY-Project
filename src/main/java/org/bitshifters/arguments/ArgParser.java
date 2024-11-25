557281fe011f0979c6a0c07e3b551e20ab62a0ba
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-25 11:16:21 +0100

    cleanup

--- src/main/java/org/bitshifters/arguments/ArgParser.java
+++ src/main/java/org/bitshifters/arguments/ArgParser.java
@@ -1,6 +1,7 @@
 package org.bitshifters.arguments;
 
 import org.bitshifters.logging.BsLogger;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
