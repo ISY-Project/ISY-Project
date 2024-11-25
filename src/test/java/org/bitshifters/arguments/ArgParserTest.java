c7c1ec19a5058359840f0a843cfe78cbf46f2b39
HEROgold <martijnwieringa28@gmail.com>
2024-11-25 21:33:29 +0100

    test argparser

--- src/test/java/org/bitshifters/arguments/ArgParserTest.java
+++ src/test/java/org/bitshifters/arguments/ArgParserTest.java
@@ -1,15 +1,21 @@
 package org.bitshifters.arguments;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
+import com.beust.jcommander.JCommander;
+
 public class ArgParserTest {
+    ArgParser argParser = new ArgParser(new String[] {"--debug"});
+
     @Test
     void testGetFlags() {
-
+        assertTrue(argParser.getFlags() instanceof Flags);
     }
 
     @Test
     void testGetJc() {
-
+        assertTrue(argParser.getJc() instanceof JCommander);
     }
 }
--- src/test/java/org/bitshifters/arguments/FlagsTest.java
+++ src/test/java/org/bitshifters/arguments/FlagsTest.java
@@ -1,4 +1,17 @@
 package org.bitshifters.arguments;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.bitshifters.enums.VerboseLevel;
+import org.junit.jupiter.api.Test;
+
 public class FlagsTest {
+    Flags flags = new Flags();
+
+    @Test
+    public void testFlags() {
+        assertTrue(flags.HELP == true || flags.HELP == false);
+        assertTrue(flags.DEBUG == true || flags.DEBUG == false);
+        assertTrue(flags.VERBOSE instanceof VerboseLevel);
+    }
 }
