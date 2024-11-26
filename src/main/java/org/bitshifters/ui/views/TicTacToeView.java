34f29e171a864ba096847cc931840f6fddff6612
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-11-26 12:21:22 +0100

    temp push

--- src/main/java/org/bitshifters/ui/views/TicTacToeView.java
+++ src/main/java/org/bitshifters/ui/views/TicTacToeView.java
@@ -1,11 +1,18 @@
 package org.bitshifters.ui.views;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import org.bitshifters.logging.BsLogger;
 import org.bitshifters.ui.MainFrame;
 import org.bitshifters.ui.componenets.BaseGrid;
 import org.bitshifters.ui.componenets.NavigationButtons;
+import org.bitshifters.ui.enums.XO;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -39,20 +46,58 @@ public class TicTacToeView extends BorderPane {
 
     public final void fillGrid() {
         logger.debug("Filling grid");
-        int buttonSize = 100;
+        int buttonSize = 200;
         Button[][] buttonGrid = this.baseGrid.getButtonGrid();
-        for (Button[] row : buttonGrid) {
-            for (Button cell : row) {
-                cell.setMinSize(buttonSize, buttonSize);
-                cell.setMaxSize(buttonSize, buttonSize);
-                cell.setStyle("-fx-font-size: 3.5em; "); // IMPORTAINT every time setStyle is called, it overrides the previous style
-                cell.setOnAction(_ -> {
-                    if (cell.getText().isEmpty()) {
-                        cell.setText("X");
-                        cell.setStyle("-fx-text-fill: #ff0000; -fx-font-size: 3.5em;");
+        for (int row = 0; row < this.baseGrid.getGridHeight(); row++) {
+            for (int col = 0; col < this.baseGrid.getGridHeight(); col++) {
+                Button button = buttonGrid[row][col];
+                button.setMinSize(buttonSize, buttonSize);
+                button.setMaxSize(buttonSize, buttonSize);
+
+                button.setStyle("-fx-font-size: 8em; "); // IMPORTAINT every time setStyle is called, it overrides the previous style
+                button.setOnAction(_ -> {
+                    if (button.getUserData() == null || button.getUserData().equals(XO.O)) {
+                        try {
+                            for (int i = 0; i < this.baseGrid.getGridHeight(); i++) {
+                                for (int j = 0; j < this.baseGrid.getGridHeight(); j++) {
+                                    Button b = buttonGrid[i][j];
+                                    if (b.equals(button)) {
+                                        var list = new ArrayList<Object>(3);
+                                        list.add(i);
+                                        list.add(j);
+                                        list.add(XO.X);
+                                        b.setUserData(list);
+                                    }
+                                }
+                            }
+                            FileInputStream input = new FileInputStream(XO.X.getRedPath());
+                            Image image = new Image(input);
+
+                            ImageView imageView = new ImageView(image);
+                            imageView.setFitHeight(buttonSize);
+                            imageView.setFitWidth(buttonSize);
+                            button.setGraphic(imageView);
+                            logger.debug("Button clicked: " + button.getUserData());
+                        } catch (FileNotFoundException e) {
+                            logger.error("Error loading image", e);
+                            button.setText("X");
+                            button.setStyle("-fx-text-fill: #ff0000; -fx-font-size: 8em;");
+                        }
                     } else {
-                        cell.setText("O");
-                        cell.setStyle("-fx-text-fill: #0000ff; -fx-font-size: 3.5em;");
+                        try {
+                            FileInputStream input = new FileInputStream(XO.O.getBluePath());
+                            Image image = new Image(input);
+
+                            ImageView imageView = new ImageView(image);
+                            imageView.setFitHeight(buttonSize);
+                            imageView.setFitWidth(buttonSize);
+                            button.setGraphic(imageView);
+                            button.setUserData(XO.O);
+                        } catch (FileNotFoundException e) {
+                            logger.error("Error loading image", e);
+                            button.setText("O");
+                            button.setStyle("-fx-text-fill: #0000ff; -fx-font-size: 8em;");
+                        }
                     }
                 });
             }
