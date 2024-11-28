9107e639e23d7f1223325ebbe9f2c824658d5784
hallojasper <159726400+hallojasper@users.noreply.github.com>
2024-11-28 01:49:28 +0100

    Remove coordinates from units

--- src/main/java/org/bitshifters/games/stratego/Unit.java
+++ src/main/java/org/bitshifters/games/stratego/Unit.java
@@ -7,8 +7,6 @@ import org.bitshifters.games.components.Player;
 public class Unit {
     private final Pawns rank;
     private Player player;
-    private int row;
-    private int col;
 
     public Unit(Pawns rank) {
         this(rank, null);
@@ -25,12 +23,6 @@ public class Unit {
     public Unit(Pawns rank, Player player, int row, int col) {
         this.rank = rank;
         this.player = player;
-        this.row = row;
-        this.col = col;
-    }
-
-    public boolean isPlaced() {
-        return row != -1 && col != -1;
     }
 
     public Pawns getRank() {
@@ -45,40 +37,8 @@ public class Unit {
         this.player = player;
     }
 
-    public int getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public void setRow(int row) {
-        setCoordinate(this.col, row);
-    }
-
-    public void setCol(int col) {
-        setCoordinate(col, this.row);
-    }
-
-    public void setCoordinate(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-
-    public void remove() {
-        this.row = -1;
-        this.col = -1;
-    }
-
-    public Unit asRotated(int totalRows, int totalCols) {
-        totalRows--;
-        totalCols--;
-        return new Unit(rank, player, totalRows - row, totalCols - col);
-    }
-
     @Override
     public String toString() {
-        return rank + " at (" + row + ", " + col + ")";
+        return rank.toString();
     }
 }
