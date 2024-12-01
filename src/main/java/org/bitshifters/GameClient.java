f40035b4c21c0341fbde2a992fdd954ab411cbc5
HEROgold <martijnwieringa28@gmail.com>
2024-12-01 13:20:43 +0100

    add player

--- src/main/java/org/bitshifters/GameClient.java
+++ src/main/java/org/bitshifters/GameClient.java
@@ -3,12 +3,14 @@ package org.bitshifters;
 import javax.swing.JFrame;
 
 import org.bitshifters.enums.GameTypes;
+import org.bitshifters.games.components.Player;
 
 public class GameClient extends JFrame {
     GameTypes gameType = null;
+    Player player;
 
-    public GameClient() {
-        
+    public GameClient(String name) {
+        this.player = new Player(name);
     }
 
     public GameTypes getGameType() {
