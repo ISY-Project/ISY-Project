23da77ec5f9cf906c9ddbb7893280612eb0af5e3
HEROgold <martijnwieringa28@gmail.com>
2024-12-04 16:03:41 +0100

    rewrite [WIP]

--- src/main/java/org/bitshifters/games/stratego/StrategoEngine.java
+++ src/main/java/org/bitshifters/games/stratego/StrategoEngine.java
@@ -22,19 +22,29 @@ public class StrategoEngine extends GridEngine<Unit> {
         this(4, 10, players);
     }
 
-    public StrategoEngine(final int playerRows, final int playerCols, final Player[] players) {
-        // TODO: Invert the playerRows and playerCols to boardRows and boardCols
-        // TODO: Add preset grids, using coordinates where lakes are placed.
+    public StrategoEngine(final int boardRows, final int boardCols, final Player[] players){
+        var playerRows = boardRows / 2 - 2;
+        var playerCols = boardCols;
+        this.totalRows = boardRows;
+        this.totalCols = boardCols;
         this.playerRows = playerRows;
         this.playerCols = playerCols;
-        // Add one to the total players to account for the movement grid.
-        // TODO: calculate the total rows and columns based on the number of players.
-        // See the generateMovementGrid method for more information on how to overlay
-        // the player grids.
-        this.totalRows = playerRows * 2 + 2;
-        this.totalCols = playerCols;
         generateGrids(players);
+        //     // TODO: Invert the playerRows and playerCols to boardRows and boardCols
+        //     // TODO: Add preset grids, using coordinates where lakes are placed.
     }
+
+    // public StrategoEngine(final int playerRows, final int playerCols, final Player[] players) {
+    //     this.playerRows = playerRows;
+    //     this.playerCols = playerCols;
+    //     // Add one to the total players to account for the movement grid.
+    //     // TODO: calculate the total rows and columns based on the number of players.
+    //     // See the generateMovementGrid method for more information on how to overlay
+    //     // the player grids.
+    //     this.totalRows = playerRows * 2 + 2;
+    //     this.totalCols = playerCols;
+    //     generateGrids(players);
+    // }
 
     public void setUnitCounts() {
         setUnitCounts(new UnitSet());
