55b23697dbbd635edb81f32be9f2926d65f3e704
Zamorak538 <166222737+Zamorak538@users.noreply.github.com>
2024-12-09 20:03:31 +0100

    Add files via upload

--- src/main/java/org/bitshifters/games/stratego/MovementTracker.java
+++ src/main/java/org/bitshifters/games/stratego/MovementTracker.java
@@ -2,23 +2,22 @@ package org.bitshifters.games.stratego;
 
 /**
  * No piece can move back and forth between the same two spaces for more than
- * three consecutive turns (two square rule).
+ * Uneven consecutive turns (two square rule).
  * Nor can a piece endlessly chase an opposing piece it has no hope of attacking
  * (more square rule).
  */
 public class MovementTracker {
     private int[][][] track = { {} };
-    private int maxRepeations = 3;
-    private int size;
+    private int maxRepetitions;
 
-    public MovementTracker(final int boardSize) {
-        this.size = boardSize;
+    public MovementTracker(final int maxRepetitions) {
+        this.maxRepetitions = maxRepetitions;
     }
 
     public void add(final int fromRow, final int fromCol, final int toRow, final int toCol) {
         int test = 0;
         final int[][] coordinate = { { fromRow, fromCol }, { toRow, toCol } };
-        if (track.length == maxRepeations) {
+        if (track.length == maxRepetitions) {
             test = 1;
         }
         final int[][][] result = new int[track.length + 1 - test][][];
@@ -35,23 +34,22 @@ public class MovementTracker {
 
     public boolean isRepeating(final int fromRow, final int fromCol, final int toRow, final int toCol) {
         // TODO check rules
-        int[][] coordinate = { { fromRow, fromCol }, { toRow, toCol } };
-        if (track.length < maxRepeations) {
-            return false;
+        int[][] futureMove = { { fromRow, fromCol }, { toRow, toCol } };
+        if (track.length < maxRepetitions) {return false;}
+        if (maxRepetitions>1 && futureMove != getPreviousMove(1)) {return false;}
+        for(int i=0; i<maxRepetitions-2; i++){
+            if (getPreviousMove(i) != getPreviousMove(i + 2)) {return false;}
         }
-        if (
-            // getPreviousMove(2)[0] == coordinate[1]
-            // && getPreviousMove(0)[0] == coordinate[1]
-            // && getPreviousMove(0)[1] == coordinate[0]
-            // && getPreviousMove(1)[1] == getPreviousMove(0)[0]
-            // && getPreviousMove(2)[1] == getPreviousMove(1)[0]
-            // alternative
-            coordinate == getPreviousMove(1)
-            && getPreviousMove(0) == getPreviousMove(2)
-            && coordinate[1] == getPreviousMove(2)[0]
-        ) {
-            return true;
-        }
-        return false;
+        if (maxRepetitions %2 == 1 && futureMove[1] != getPreviousMove(maxRepetitions - 1)[0]) {return false;}
+        return true;
+        // old version
+        // if (
+        //     futureMove == getPreviousMove(1)
+        //     && getPreviousMove(0) == getPreviousMove(2)
+        //     && futureMove[1] == getPreviousMove(2)[0]
+        // ) {
+        //     return true;
+        // }
+        // return false;
     }
 }
