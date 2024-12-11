9c48c0a0a79758262e2616a501d6e05126072e61
HEROgold <martijnwieringa28@gmail.com>
2024-12-11 14:16:22 +0100

    don't write config

--- src/main/java/org/bitshifters/Main.java
+++ src/main/java/org/bitshifters/Main.java
@@ -34,7 +34,7 @@ public class Main{
         final Main main = new Main();
         configureArgParser(args);
         configureLogging();
-        config.write(); // instantly write the config, with default values.
+        // config.write(); // instantly write the config, with default values.
         main.run(args);
         MainFrame.run(args);
         ClientController clientController = new ClientController(config.getValue("username"));
