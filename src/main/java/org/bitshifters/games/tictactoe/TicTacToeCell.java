26e067468a7cd7fc368a8151dc4958bc6c977d0a
sander1946 <71840186+sander1946@users.noreply.github.com>
2024-12-12 14:04:55 +0100

    added autogen javaDocs to the tictactoe game

--- src/main/java/org/bitshifters/games/tictactoe/TicTacToeCell.java
+++ src/main/java/org/bitshifters/games/tictactoe/TicTacToeCell.java
@@ -1,5 +1,8 @@
 package org.bitshifters.games.tictactoe;
 
+/**
+ * Enum for the different types of cells in TicTacToe.
+ */
 public enum TicTacToeCell {
     X("X"), 
     O("O"),
@@ -8,18 +11,34 @@ public enum TicTacToeCell {
     private final String typeChar;
     private final String path = "src\\main\\resources\\images";
 
+    /**
+     * Constructor for TicTacToeCell.
+     * @param typeChar the type of cell
+     */
     TicTacToeCell(String typeChar) {
         this.typeChar = typeChar;
     }
 
+    /**
+     * Get the type of cell.
+     * @return the type of cell
+     */
     public String getChar() {
         return typeChar;
     }
 
+    /**
+     * Get the path for the red cell.
+     * @return the path for the red cell
+     */
     public String getRedPath() {
         return path + "\\Red" + typeChar + ".png";
     }
 
+    /**
+     * Get the path for the blue cell.
+     * @return the path for the blue cell
+     */
     public String getBluePath() {
         return path + "\\Blue" + typeChar + ".png";
     }
