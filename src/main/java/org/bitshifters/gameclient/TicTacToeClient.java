4b52b7011dd2b47ded35e59d6e3a6dccfb663030
HEROgold <martijnwieringa28@gmail.com>
2024-12-12 13:08:57 +0100

    add javadoc

--- src/main/java/org/bitshifters/gameclient/TicTacToeClient.java
+++ src/main/java/org/bitshifters/gameclient/TicTacToeClient.java
@@ -15,12 +15,10 @@ import org.bitshifters.telnet.TelnetClient;
 import org.bitshifters.ui.views.TicTacToeView;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventType;
 
 /**
- * CLient moet online kunnen spelen
- * Client moet tegen Ai kunnen spelen
- * 
+ * Client for playing Tic-Tac-Toe.
+ * Can play online or against an AI.
  */
 public class TicTacToeClient extends GameClient {
     private static final BSLogger logger = new BSLogger(TicTacToeClient.class);
@@ -30,6 +28,12 @@ public class TicTacToeClient extends GameClient {
     private final TicTacToeView view;
     private final TTTEngine engine;
 
+    /**
+     * Constructs a TicTacToeClient.
+     * @param view The TicTacToe view.
+     * @param playerX The player X.
+     * @param playerO The player O.
+     */
     public TicTacToeClient(TicTacToeView view, Player playerX, Player playerO) {
         super(GameTypes.TicTacToe);
         telnet = new TelnetClient(
@@ -45,6 +49,10 @@ public class TicTacToeClient extends GameClient {
         setupGridButtonListeners(view);
     }
 
+    /**
+     * Sets up the button listeners for the grid.
+     * @param view The TicTacToe view.
+     */
     private void setupGridButtonListeners(TicTacToeView view) {
         for (int row = 0; row < view.getBaseGrid().getButtonGrid().length; row++) {
             for (int col = 0; col < view.getBaseGrid().getButtonGrid()[row].length; col++) {
@@ -57,6 +65,12 @@ public class TicTacToeClient extends GameClient {
         }
     }
 
+    /**
+     * Handles the button click event.
+     * @param view The TicTacToe view.
+     * @param finalRow The row of the clicked button.
+     * @param finalCol The column of the clicked button.
+     */
     private void handleButtonClick(TicTacToeView view, final int finalRow, final int finalCol) {
         logger.info("Button clicked at row: " + finalRow + " col: " + finalCol);
         int index = GT.toIndex(finalRow, finalCol, view.getBaseGrid().getButtonGrid().length);
@@ -66,10 +80,10 @@ public class TicTacToeClient extends GameClient {
     }
 
     /**
-     * Make a move on the board
-     * @param row
-     * @param col
-     * @return
+     * Makes a move on the board.
+     * @param row The row of the move.
+     * @param col The column of the move.
+     * @return True if the move is valid, false otherwise.
      */
     public boolean makeMove(int row, int col) {
         logger.info("Making move at row: " + row + " col: " + col);
@@ -88,8 +102,8 @@ public class TicTacToeClient extends GameClient {
     }
 
     /**
-     * Get the next player
-     * @return
+     * Gets the next player.
+     * @return The next player.
      */
     private Player getNextPlayer() {
         return engine.getActivePlayer() == engine.getPlayerX() ? engine.getPlayerX() : engine.getPlayerO();
@@ -125,6 +139,11 @@ public class TicTacToeClient extends GameClient {
         makeMove(bestMove / 3, bestMove % 3);
     }
 
+    /**
+     * Converts the grid to a char array.
+     * @param grid The TicTacToe grid.
+     * @return The char array representation of the grid.
+     */
     private char[] getCharGrid(Grid<TicTacToeCell> grid) {
         char[] charGrid = new char[9];
         for (int i = 0; i < grid.getRowCount(); i++) {
