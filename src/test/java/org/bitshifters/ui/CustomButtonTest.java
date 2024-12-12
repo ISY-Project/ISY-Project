630a74dea883f7e2a6fd8cddbb6645eb6c8146e6
HEROgold <martijnwieringa28@gmail.com>
2024-12-12 13:57:13 +0100

    add line

--- src/test/java/org/bitshifters/ui/CustomButtonTest.java
+++ src/test/java/org/bitshifters/ui/CustomButtonTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import javafx.application.Platform;
 
 public class CustomButtonTest {
