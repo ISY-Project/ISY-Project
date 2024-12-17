f1b527cc0a9a2063a1516b99fa8b65f08900d932
HEROgold <martijnwieringa28@gmail.com>
2024-12-17 15:52:12 +0100

    Doc: addexd. Added: Place for stratego

--- src/main/java/org/bitshifters/telnet/Commands/Place.java
+++ src/main/java/org/bitshifters/telnet/Commands/Place.java
@@ -3,8 +3,22 @@ package org.bitshifters.telnet.Commands;
 public class Place implements SendableCommand {
     private final String command;
 
+    /**
+     * Constructor for Place command for the stratego unit placement
+     * @param start_index
+     * @param end_index
+     */
     public Place(int start_index, int end_index) {
         this.command = "place " + start_index + " " + end_index;
+    }
+
+    /**
+     * Constructor for Place command for the stratego unit placement
+     * @param rank
+     * @param index
+     */
+    public Place(String rank, int index) {
+        this.command = "place " + rank + " " + index;
     }
 
     @Override
