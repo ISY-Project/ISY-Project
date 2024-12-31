ab0b40317c335b60833d65bce77db70fcff34cd6
HEROgold <martijnwieringa28@gmail.com>
2024-12-31 15:13:13 +0100

    Changes: usage of enums instead of strings

--- src/main/java/org/bitshifters/telnet/Commands/Subscribe.java
+++ src/main/java/org/bitshifters/telnet/Commands/Subscribe.java
@@ -1,18 +1,28 @@
 package org.bitshifters.telnet.Commands;
 
+import org.bitshifters.games.GameTypes;
+
 public enum Subscribe implements SendableCommand {
-    BATTLESHIP("battleship"),
-    TTT("tic-tac-toe"),
-    STRATEGO("stratego"),;
+    BATTLESHIP(GameTypes.BattleShip),
+    TICTACTOE(GameTypes.TicTacToe),
+    STRATEGO(GameTypes.Stratego),;
 
     private final String game;
 
-    Subscribe(String game) {
-        this.game = "subscribe " + game.toLowerCase();
+    Subscribe(GameTypes game) {
+        if (game == GameTypes.TicTacToe) {
+            this.game = "subscribe tic-tac-toe";
+            return;
+        }
+        this.game = "subscribe " + game.toString().toLowerCase();
     }
 
     @Override
     public String get() {
         return this.game;
+    }
+    
+    public static Subscribe fromGameType(GameTypes game) {
+        return Subscribe.valueOf(game.toString().toUpperCase());
     }
 }
