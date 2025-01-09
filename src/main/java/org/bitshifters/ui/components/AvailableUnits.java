a5a02ebebf53a1d1c9b4c898d9ba5c89a075d168
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-01-09 09:11:44 +0100

    sorted the avalible units buttons

--- src/main/java/org/bitshifters/ui/components/AvailableUnits.java
+++ src/main/java/org/bitshifters/ui/components/AvailableUnits.java
@@ -2,6 +2,7 @@ package org.bitshifters.ui.components;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Set;
 import java.util.logging.Level;
 
 import org.bitshifters.games.stratego.Pawns;
@@ -61,7 +62,8 @@ public final class AvailableUnits extends HBox {
      */
     private void createButtons() {
         int i = 0;
-        for (final Pawns pawn : this.availableUnits.getUnits().keySet()) {
+
+        for (final Pawns pawn : getSortedPawns()) {
             buttons[i] = new Button();
             buttons[i].setPrefSize(buttonWidth, buttonHeight);
             buttons[i].setStyle(style);
@@ -98,6 +100,25 @@ public final class AvailableUnits extends HBox {
 
             i++;
         }
+    }
+
+    private Pawns[] getSortedPawns() {
+        // TODO: make this more efficient its O(n^2)
+        Pawns[] pawns = new Pawns[this.availableUnits.getUnits().size()];
+        Set<Pawns> keyset = this.availableUnits.getUnits().keySet();
+        Pawns[] pawnss = Pawns.values();
+
+        int i = 0;
+        for (Pawns pawn : pawnss) {
+            // check if the pawn is in the pawnss array
+            for (Pawns p : keyset) {
+                if (p.equals(pawn)) {
+                    pawns[i] = pawn;
+                    i++;
+                }
+            }
+        }
+        return pawns;
     }
 
     /**
