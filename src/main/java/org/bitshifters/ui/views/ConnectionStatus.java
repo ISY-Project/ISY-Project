3fa03bd9e2d767de0a395f9915712fb9b36320bf
HEROgold <martijnwieringa28@gmail.com>
2025-01-09 12:01:49 +0100

    add handler

--- src/main/java/org/bitshifters/ui/views/ConnectionStatus.java
+++ src/main/java/org/bitshifters/ui/views/ConnectionStatus.java
@@ -1,16 +1,21 @@
 package org.bitshifters.ui.views;
 
 import org.bitshifters.ClientController;
+import org.bitshifters.telnet.OnConnectionSwitch;
 import org.bitshifters.telnet.TelnetClient;
 import org.bitshifters.ui.MainFrame;
 import org.bitshifters.ui.components.CustomBorderPane;
 
-public class ConnectionStatus extends CustomBorderPane {
+public class ConnectionStatus extends CustomBorderPane implements OnConnectionSwitch {
     private static final TelnetClient telnet = ClientController.telnet;
 
     public ConnectionStatus(MainFrame mainFrame) {
         super(mainFrame);
     }
 
-    
+    @Override
+    public void handle(boolean isConnected) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handle'");
+    }
 }
